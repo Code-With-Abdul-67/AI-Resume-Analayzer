c3de10fa9d8f3ad1d853c915a1d37d1d79431338
Code-With-Abduul-67 <codewithabdul67@gmail.com>
2026-02-12 23:59:48 +0500

    feat: Improve `signIn` callback logging with environment variable checks and detailed user/provider information.

--- src/backend/auth.ts
+++ src/backend/auth.ts
@@ -14,15 +14,21 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         }),
     ],
     callbacks: {
+        async signIn({ user, account, profile }) {
+            console.log("Check Env:", {
+                secret: !!process.env.AUTH_SECRET,
+                googleId: !!process.env.AUTH_GOOGLE_ID,
+                trustHost: !!process.env.AUTH_TRUST_HOST,
+                url: !!process.env.AUTH_URL
+            });
+            console.log("Sign-in attempt:", { user: user?.email, provider: account?.provider });
+            return true;
+        },
         session({ session, user }) {
             if (session.user) {
                 session.user.id = user.id;
             }
             return session;
-        },
-        async signIn({ user, account, profile }) {
-            console.log("Sign-in attempt:", { user, account, profile });
-            return true;
         },
     },
 })
